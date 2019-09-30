c45c7c3c899f6b77294dffae31724feaa4c9fde3
vehiclefleetmanager <tmm.miernik@gmail.com>
2019-09-30 14:47:19 +0200

    extending the application for the Swagger UI tool

--- src/main/java/com/example/springhomework/week3/Week3Application.java
+++ src/main/java/com/example/springhomework/week3/Week3Application.java
@@ -2,12 +2,27 @@ package com.example.springhomework.week3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Week3Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Week3Application.class, args);
     }
 
+    @Bean
+    public Docket getDocket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
